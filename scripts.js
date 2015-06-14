14ca178e676924e0702d227520b0e2093b26cc47
jdmcpeek <david.mcpeek@yale.edu>
2015-06-14 07:00:41 -0700

    trying the clicked route, nada

--- scripts.js
+++ scripts.js
@@ -7,3 +7,23 @@ function duplicateElement(object)
 {
 	document.body.innerHTML += object.innerHTML;
 }
+
+clicked = false;
+
+$(".testbox").on("click", function() {
+
+	// how do I delay this next function from happening until the next click? Do I need to stop propagation or something?
+	clicked = true;
+
+
+});
+
+if (clicked == true) 
+{
+	$("body").on("click", function() {
+		
+			$(".testbox").css({"left": event.pageX - 40, "top": event.pageY -40});
+			clicked = false;	
+
+	});
+}
